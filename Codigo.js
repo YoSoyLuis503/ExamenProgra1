adbb4d7f4252dfcddf2750eb8921e573c326e8a8
JonaC7 <109814169+JonaC7@users.noreply.github.com>
2024-02-19 08:23:14 -0600

    img mauseover

--- Codigo.js
+++ Codigo.js
@@ -1,7 +1,7 @@
-/*function cambiarImagen(nuevaImagen) {
+function cambiarImagen(nuevaImagen) {
     document.getElementById('miImagen').src = nuevaImagen;
 }
 
 function restaurarImagen(imagenOriginal) {
     document.getElementById('miImagen').src = imagenOriginal;
-}*/
+}
