5853d018445e1cbf7c84ff7704aee79e9f1e030d
JonaC7 <109814169+JonaC7@users.noreply.github.com>
2024-02-19 08:14:20 -0600

    Img

---
+++ Codigo.js
@@ -0,0 +1,7 @@
+/*function cambiarImagen(nuevaImagen) {
+    document.getElementById('miImagen').src = nuevaImagen;
+}
+
+function restaurarImagen(imagenOriginal) {
+    document.getElementById('miImagen').src = imagenOriginal;
+}*/
